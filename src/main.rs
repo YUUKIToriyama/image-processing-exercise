16ee4a08c8ba65cdbadd91e6826e912093704c85
YUUKIToriyama <github@toriyama.dev>
2022-11-03 11:38:59 +0900

    サンプルコードを追加

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-mod scale;
 mod affine;
+mod scale;
 
-use scale::enlarge_image;
+use affine::rotate_image;
+use std::f32::consts::PI;
 
 fn main() {
     let scale: u32 = 3;
-    let img = image::open("./sample.png").unwrap();
-    let enlarged_img = enlarge_image(img, scale);
-    enlarged_img.save("./generated.png").unwrap();
+    let mut img = image::open("./sample.png").unwrap();
+    let rotated_img = rotate_image(img, PI / 6.0);
+    rotated_img.save("./generated.png").unwrap();
 }
